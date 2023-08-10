7adf2132b949a661bea829a7763d18f3926f614e
tima3377 <peoplecanfly>
2023-08-10 12:15:48 +0300

    angular crash course init

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
+  showAddTask: boolean
+  subscription: Subscription
+
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask()
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route
+  }
+}
